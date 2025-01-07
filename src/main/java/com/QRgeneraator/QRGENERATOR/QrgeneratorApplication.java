b997ce57e28df5b2e73dcbfa52275f6cfff333d3
VICTOR ANDREE SEVILLANO VEGA <U21221311@utp.edu.pe>
2025-01-06 22:32:09 -0500

    Subiendo los primeros cambios, actualizacion el POM con independeacias del QR

---
+++ src/main/java/com/QRgeneraator/QRGENERATOR/QrgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.QRgeneraator.QRGENERATOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QRgeneraator/QRGENERATOR/QrgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QRgeneraator.QRGENERATOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
